55762ba509d1ba181c5e596c4331500cf6c7eb6f
IsmailZaman <ismailzaman70@gmail.com>
2022-05-23 20:21:04 +0500

    minor fix: Layout prop added

--- src/components/pages/sessions/CourseForm/AddCoursesForm.js
+++ src/components/pages/sessions/CourseForm/AddCoursesForm.js
@@ -2,6 +2,7 @@ import CoursesList from './CoursesList';
 import Layout from '../../../layout/Layout';
 import useFetch from '../../../../hooks/useFetch';
 import Loading from '../../../reusable-components/Loading';
+import adminNavbarLinks from '../../../layout/admin-navlinks';
 
 
 
@@ -16,7 +17,7 @@ export default function AddCoursesForm() {
 
   return (
     
-    <Layout title={apiData ? apiData?.data?.name : "Active Session not found" }>
+    <Layout title={apiData ? apiData?.data?.name : "Active Session not found"} navlinks={adminNavbarLinks}>
         {loading && <Loading/>}
         {!loading &&( apiData && <CoursesList/>) }
         {error && <h1>No Active Session Found.</h1>}
